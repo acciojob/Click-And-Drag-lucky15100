99019a6d8c6f96a723b36b54419700fadf92f544
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-21 22:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Your code here.
+<script>
+  const items = document.querySelector('.items');
+  let isMouseDown = false;
+  let startX;
+  let scrollLeft;
+
+  items.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
+    items.classList.add('active');
+    startX = e.pageX - items.offsetLeft;
+    scrollLeft = items.scrollLeft;
+  });
+
+  items.addEventListener('mouseleave', () => {
+    isMouseDown = false;
+    items.classList.remove('active');
+  });
+
+  items.addEventListener('mouseup', () => {
+    isMouseDown = false;
+    items.classList.remove('active');
+  });
+
+  items.addEventListener('mousemove', (e) => {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - items.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust the multiplier for faster/slower scrolling
+    items.scrollLeft = scrollLeft - walk;
+  });
+</script>
